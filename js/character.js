6d9189169b06a9006b645265f40c6f83db5b6c0d
wndudrla1011 <wndudrla1011@naver.com>
2021-12-19 12:16:56 +0900

    Create page_arrangement

--- js/character.js
+++ js/character.js
@@ -10,9 +10,25 @@ function Character(info) {
 
   this.mainElem.firstChild.style.borderBottom = `6rem solid ${info.color}`;
   document.querySelector(".stage").appendChild(this.mainElem);
+
+  this.lastScrollPos = 0;
+  this.init();
 }
 
 Character.prototype = {
   constructor: Character,
-  // init:
+  init: function () {
+    const obj = this; //this -> character
+    window.addEventListener("scroll", function () {
+      if (obj.lastScrollPos < pageYOffset) {
+        //캐릭터 뒷면
+        obj.mainElem.setAttribute("data-direction", "forward");
+      } else if (obj.lastScrollPos > pageYOffset) {
+        //캐릭터 앞면
+        obj.mainElem.setAttribute("data-direction", "backward");
+      }
+
+      obj.lastScrollPos = pageYOffset;
+    }); //scroll event end
+  }, //init end
 };
--- js/main-board.js
+++ js/main-board.js
@@ -2,6 +2,17 @@
   const stageElem = document.querySelector(".stage");
   const houseElem = document.querySelector(".house");
   const colors = ["#57c9f2", "#e80000", "#3809b6", "#29c92b", "#f76801"];
+  let maxScrollValue;
+
+  function reallocate() {
+    maxScrollValue = document.body.offsetHeight - window.innerHeight;
+  }
+
+  window.addEventListener("scroll", function () {
+    const scrollValue = pageYOffset / maxScrollValue;
+    const zRange = scrollValue * 430 - 200;
+    houseElem.style.transform = "translateZ(" + zRange + "vw)";
+  });
 
   stageElem.addEventListener("click", function (e) {
     let index = Math.floor(Math.random() * 5);
@@ -11,4 +22,8 @@
       color: colors[index],
     });
   });
+
+  window.addEventListener("resize", reallocate);
+
+  reallocate();
 })();
