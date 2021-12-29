36863e9d84c08bb78731de2f5da3cafd97873953
wndudrla1011 <wndudrla1011@naver.com>
2021-12-29 11:16:15 +0900

    completed open_doors

---
+++ js/login.js
@@ -0,0 +1,47 @@
+(function () {
+  const loginForm = document.querySelector("#login-form");
+  const loginInput = document.querySelector("#login-form input");
+  const greetings = document.querySelector("#greetings");
+  const moveButton = document.querySelector("#move-button");
+  const yolk = document.querySelector("#move-botton__sub");
+  const h2 = document.querySelector("#todo-list__form h2");
+
+  const HIDDEN_CLASSNAME = "hidden";
+  const USERNAME_KEY = "username";
+
+  function onLoginSubmit(event) {
+    event.preventDefault();
+    loginForm.classList.add(HIDDEN_CLASSNAME);
+    const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY, username);
+    paintGreetings(username);
+  }
+
+  function paintGreetings(username) {
+    const span = document.createElement("span");
+    const hello = document.createElement("span");
+    const userName = document.createElement("span");
+    yolk.appendChild(span);
+    greetings.appendChild(hello);
+    greetings.appendChild(userName);
+    // hello.innerText = "Hello";
+    // userName.innerText = username;
+    hello.innerText = "Hello";
+    userName.innerText = username;
+    span.innerText = "Manage Yours ▶ Main";
+    h2.innerText = `${username}'s To Do List!`;
+    hello.classList.remove(HIDDEN_CLASSNAME);
+    userName.classList.remove(HIDDEN_CLASSNAME);
+    moveButton.classList.remove(HIDDEN_CLASSNAME);
+    yolk.classList.remove(HIDDEN_CLASSNAME);
+  }
+
+  const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+  if (savedUsername === null) {
+    loginForm.classList.remove(HIDDEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+  } else {
+    paintGreetings(savedUsername);
+  }
+})();
--- js/main-board.js
+++ js/main-board.js
@@ -1,18 +1,42 @@
 (function () {
   const stageElem = document.querySelector(".stage");
   const houseElem = document.querySelector(".house");
+  const door1 = document.querySelector(".page_front-login");
+  const door2 = document.querySelector(".page_front-main");
   const colors = ["#57c9f2", "#e80000", "#3809b6", "#29c92b", "#f76801"];
   const mouseXY = { x: 0, y: 0 };
   let maxScrollValue;
+  const USERNAME_KEY = "username";
 
   function reallocate() {
     maxScrollValue = document.body.offsetHeight - window.innerHeight;
   }
 
-  window.addEventListener("scroll", function () {
+  function open(object) {
+    object.classList.add("opened");
+  }
+
+  function close(object) {
+    object.classList.remove("opened");
+  }
+
+  window.addEventListener("scroll", function (e) {
     const scrollValue = pageYOffset / maxScrollValue;
     const zRange = scrollValue * 430 - 200;
     houseElem.style.transform = "translateZ(" + zRange + "vw)";
+    const savedUsername = localStorage.getItem(USERNAME_KEY);
+    if (savedUsername != null) {
+      if (scrollValue > 0.31) {
+        open(door1);
+      } else {
+        close(door1);
+      }
+      if (scrollValue > 0.7) {
+        open(door2);
+      } else {
+        close(door2);
+      }
+    }
   });
 
   stageElem.addEventListener("click", function (e) {
@@ -28,7 +52,7 @@
     mouseXY.x = -1 + (e.clientX / window.innerWidth) * 2;
     mouseXY.y = 1 - (e.clientY / window.innerHeight) * 2;
     stageElem.style.transform =
-      "rotateX(" + mouseXY.y * 5 + "deg) rotateY(" + mouseXY.x * 5 + "deg)";
+      "rotateX(" + mouseXY.y * 10 + "deg) rotateY(" + mouseXY.x * 5 + "deg)";
   });
 
   window.addEventListener("resize", reallocate);
