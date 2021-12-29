6904ebca8bd92855876f010094ca41f98778c638
wndudrla1011 <wndudrla1011@naver.com>
2021-12-29 13:11:27 +0900

    completed scroll_block

--- js/main-board.js
+++ js/main-board.js
@@ -3,10 +3,19 @@
   const houseElem = document.querySelector(".house");
   const door1 = document.querySelector(".page_front-login");
   const door2 = document.querySelector(".page_front-main");
+  const loginForm = document.querySelector("#login-form");
   const colors = ["#57c9f2", "#e80000", "#3809b6", "#29c92b", "#f76801"];
   const mouseXY = { x: 0, y: 0 };
+  //최대 스크롤 크기
   let maxScrollValue;
+  //submit 상태인 지 체크
+  let activate;
+  //초기 스크롤 위치 값
+  let initialScroll;
+  //setInterval return value
+  let intervalId;
   const USERNAME_KEY = "username";
+  const savedUsername = localStorage.getItem(USERNAME_KEY);
 
   function reallocate() {
     maxScrollValue = document.body.offsetHeight - window.innerHeight;
@@ -20,12 +29,30 @@
     object.classList.remove("opened");
   }
 
+  //0.5초마다 초기 스크롤 위치 계산
+  intervalId = setInterval(function () {
+    initialScroll = pageYOffset / maxScrollValue;
+    if (initialScroll > 0.2) {
+      if (savedUsername === null) {
+        document.body.classList.add("block");
+        activate = false;
+      }
+    }
+  }, 500);
+
+  //username 제출 시, setInterval을 중지시키고 scroll block 해제
+  loginForm.addEventListener("submit", function () {
+    clearInterval(intervalId);
+    document.body.classList.remove("block");
+    activate = true;
+  });
+
   window.addEventListener("scroll", function (e) {
     const scrollValue = pageYOffset / maxScrollValue;
     const zRange = scrollValue * 430 - 200;
     houseElem.style.transform = "translateZ(" + zRange + "vw)";
-    const savedUsername = localStorage.getItem(USERNAME_KEY);
-    if (savedUsername != null) {
+
+    if (activate) {
       if (scrollValue > 0.31) {
         open(door1);
       } else {
