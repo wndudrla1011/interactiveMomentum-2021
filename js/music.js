a56e791352a7a3da1be8ebb414590c6b70efa1af
wndudrla1011 <wndudrla1011@naver.com>
2021-12-31 19:55:55 +0900

    completed stick_ani

--- js/music.js
+++ js/music.js
@@ -1,7 +1,16 @@
 (function () {
   const backgroundMusic = document.querySelector(".background-music");
   const randomMusic = document.querySelector(".random-music");
-  const sticks = document.querySelector(".animation-gruop__stick");
+  const sticks = [
+    "stick-one",
+    "stick-two",
+    "stick-three",
+    "stick-four",
+    "stick-five",
+    "stick-six",
+    "stick-seven",
+    "stick-eight",
+  ];
   const playlist = [
     "music1.mp3",
     "music2.mp3",
@@ -14,6 +23,7 @@
     "music9.mp3",
     "music10.mp3",
   ];
+  let i;
 
   function playMusic() {
     backgroundMusic.volume = 0.5;
@@ -24,7 +34,17 @@
   // window.addEventListener("beforeunload", playMusic);
 
   backgroundMusic.addEventListener("play", function () {
-    sticks.classList.add("play");
+    for (i = 0; i < sticks.length; i++) {
+      const stick = sticks[i];
+      document.querySelector(`.${stick}`).classList.add("play");
+    }
+  });
+
+  backgroundMusic.addEventListener("pause", function () {
+    for (i = 0; i < sticks.length; i++) {
+      const stick = sticks[i];
+      document.querySelector(`.${stick}`).classList.remove("play");
+    }
   });
 
   randomMusic.addEventListener("click", function () {
